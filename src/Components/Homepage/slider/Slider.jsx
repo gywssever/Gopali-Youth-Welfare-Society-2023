31fae11607c5b3926fda5baa449ed4dc97128249
Ashutosh Kr Singh <ashutoshkrsinghsm@gmail.com>
2023-10-15 19:17:14 +0530

    Improved Footer width and positioning

--- src/Components/Homepage/slider/Slider.jsx
+++ src/Components/Homepage/slider/Slider.jsx
@@ -16,7 +16,7 @@ function Slider() {
         >
           <div>
             <img src={header_bg_1} alt='JVM Students' className='carousel_image' />
-            <p className="legend">JVM students</p>
+            <p className="legend">Students at Jagriti Vidya Mandir(JVM)</p>
           </div>
           <div>
             <img src={JVM} alt='We with the students' className='carousel_image' />
@@ -25,12 +25,6 @@ function Slider() {
           </div>
         </Carousel>
 
-        {/* <div className='carousel_heading'>
-          IITian's empowering youth for BETTER TOMORROW
-        </div> */}
-        {/* <div className="carousel_donate">
-            <button className="btn">Yes, I want to Donate</button>
-        </div> */}
       </div>
 
     </>
