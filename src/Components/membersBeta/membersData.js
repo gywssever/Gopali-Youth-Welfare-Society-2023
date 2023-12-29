ef1bc27e2c5a519bde06fa7b0c51971978265ca9
Ashutosh Kr Singh <ashutoshkrsinghsm@gmail.com>
2023-12-29 10:16:50 +0530

    membersBeta

---
+++ src/Components/membersBeta/membersData.js
@@ -0,0 +1,490 @@
+const allData = [
+    {
+        id: 1,
+        year: 2019,
+        governingBody: [
+            {
+                srNo: 1,
+                name: 'Mrinal Kanti Bhanja',
+                position: 'PRESIDENT',
+                image: "imgLink",
+                linkedIn: "link",
+                facebook: "link",
+                email: "link",
+            },
+            {
+                srNo: 2,
+                name: 'Mr. Prerit Jain',
+                position: 'GENERAL SECRETARY',
+                image: "imgLink",
+                linkedIn: "link",
+                facebook: "link",
+                email: "link",
+
+            },
+            {
+                srNo: 4,
+                name: 'Mr. Karan Nandankar',
+                position: 'ASSISTANT SECRETARY',
+                image: "imgLink",
+                linkedIn: "link",
+                facebook: "link",
+                email: "link",
+
+            },
+            {
+                srNo: 5,
+                name: 'Mr. Ankit Raj',
+                position: 'TREASURER',
+                image: "imgLink",
+                linkedIn: "link",
+                facebook: "link",
+                email: "link",
+
+            },
+            {
+                srNo: 6,
+                name: 'Mr. Ishan',
+                position: 'PUBLIC RELATIONS OFFICER',
+                image: "imgLink",
+                linkedIn: "link",
+                facebook: "link",
+                email: "link",
+
+            },
+            {
+                srNo: 7,
+                name: 'Mr. Prabhat Ranjan',
+                position: 'SCHOOL DEVELOPMENT OFFICER',
+                image: "imgLink",
+                linkedIn: "link",
+                facebook: "link",
+                email: "link",
+
+            },
+            {
+                srNo: 8,
+                name: 'Mr. Kumar Aniket',
+                position: 'CHIEF FUNDRAISING OFFICER',
+                image: "imgLink",
+                linkedIn: "link",
+                facebook: "link",
+                email: "link",
+
+            },
+            {
+                srNo: 9,
+                name: 'Mr. Gursharan Ahir',
+                position: 'HUMAN RESOURCE MANAGER',
+                image: "imgLink",
+                linkedIn: "link",
+                facebook: "link",
+                email: "link",
+
+            },
+            {
+                srNo: 10,
+                name: 'Mr. Gaurav Singh',
+                position: ' EXECUTIVE OFFICER [RS]',
+                image: "imgLink",
+                linkedIn: "link",
+                facebook: "link",
+                email: "link",
+
+            },
+            {
+                srNo: 11,
+                name: 'Mr. Manoj Khojaram Maraskolhe',
+                position: 'CHIEF EXECUTIVE OFFICER [LiGHT]',
+                image: "imgLink",
+                linkedIn: "link",
+                facebook: "link",
+                email: "link",
+
+            },
+        ],
+        advisoryBody: [
+            {
+                srNo: 1,
+                name: "Prof. Adrijit Goswami",
+                position: "",
+                imageUrl: "/gyws/assets/img/new_website_img/members/Prof. Adrijit Goswami.jpg",
+                facebookLink: "",
+                linkedinLink: ""
+            },
+            {
+                srNo: 2,
+                name: "Prof. Amit Patra",
+                position: "",
+                imageUrl: "/gyws/assets/img/new_website_img/members/Prof. Amit Patra.jpg",
+                facebookLink: "",
+                linkedinLink: ""
+            },
+            {
+                srNo: 3,
+                name: "Dr. Bhaskar Bhowmik",
+                position: "",
+                imageUrl: "/gyws/assets/img/new_website_img/members/Dr. Bhaskar Bhowmik.jpg",
+                facebookLink: "",
+                linkedinLink: ""
+            },
+            {
+                srNo: 4,
+                name: "Prof. Damodar Maiti",
+                position: "",
+                imageUrl: "/gyws/assets/img/new_website_img/members/Prof. Damodar Maiti.jpg",
+                facebookLink: "",
+                linkedinLink: ""
+            },
+            {
+                srNo: 5,
+                name: "Prof. D.K. Maiti",
+                position: "",
+                imageUrl: "/gyws/assets/img/new_website_img/members/Prof. D.K. Maiti.jpg",
+                facebookLink: "",
+                linkedinLink: ""
+            },
+            {
+                srNo: 6,
+                name: "Prof. Dilip Swain",
+                position: "",
+                imageUrl: "/gyws/assets/img/new_website_img/members/Prof. Dilip Swain.jpg",
+                facebookLink: "",
+                linkedinLink: ""
+            },
+            {
+                srNo: 7,
+                name: "Retd. Prof. H.R. Tiwari",
+                position: "",
+                imageUrl: "/gyws/assets/img/new_website_img/members/dummy.png",
+                facebookLink: "",
+                linkedinLink: ""
+            },
+            {
+                srNo: 8,
+                name: "Prof. Sasmita Mohakud",
+                position: "",
+                imageUrl: "/gyws/assets/img/new_website_img/members/dummy.png",
+                facebookLink: "",
+                linkedinLink: ""
+            },
+            {
+                srNo: 9,
+                name: "Prof. Sudipta Datta",
+                position: "",
+                imageUrl: "/gyws/assets/img/new_website_img/members/dummy.png",
+                facebookLink: "",
+                linkedinLink: ""
+            },
+            {
+                srNo: 10,
+                name: "Prof. Sudipto Chakraborty",
+                position: "",
+                imageUrl: "/gyws/assets/img/new_website_img/members/Prof. Sudipto Chakraborty.jpg",
+                facebookLink: "",
+                linkedinLink: ""
+            },
+        ],
+        jagritiVidyaMandir: [
+            {
+                srNo: 1,
+                coordinators: [
+                    {
+                        srNo: 1,
+                        name: "",
+                        imageUrl: "link",
+                        facebookLink: "link",
+                        linkedinLink: "link",
+                        email: "link"
+                    },
+                    {
+                        srNo: 1,
+                        name: "",
+                        imageUrl: "link",
+                        facebookLink: "link",
+                        linkedinLink: "link",
+                        email: "link"
+                    },
+                    {
+                        srNo: 1,
+                        name: "",
+                        imageUrl: "link",
+                        facebookLink: "link",
+                        linkedinLink: "link",
+                        email: "link"
+                    },
+                ],
+                schoolReviewCommittee: [
+                    {
+                        srNo: 1,
+                        name: "",
+                        imageUrl: "link",
+                        facebookLink: "link",
+                        linkedinLink: "link",
+                        email: "link"
+                    },
+                    {
+                        srNo: 1,
+                        name: "",
+                        imageUrl: "link",
+                        facebookLink: "link",
+                        linkedinLink: "link",
+                        email: "link"
+                    },
+                    {
+                        srNo: 1,
+                        name: "",
+                        imageUrl: "link",
+                        facebookLink: "link",
+                        linkedinLink: "link",
+                        email: "link"
+                    },
+                ]
+            }
+        ]
+    },
+    {
+        id: 2,
+        year: 2020,
+        governingBody: [
+            {
+                srNo: 1,
+                name: 'Mrinal Kanti Bhanja',
+                position: 'PRESIDENT',
+                image: "imgLink",
+                linkedIn: "link",
+                facebook: "link",
+                email: "link",
+            },
+            {
+                srNo: 2,
+                name: 'Mr. Prerit Jain',
+                position: 'GENERAL SECRETARY',
+                image: "imgLink",
+                linkedIn: "link",
+                facebook: "link",
+                email: "link",
+
+            },
+            {
+                srNo: 4,
+                name: 'Mr. Karan Nandankar',
+                position: 'ASSISTANT SECRETARY',
+                image: "imgLink",
+                linkedIn: "link",
+                facebook: "link",
+                email: "link",
+
+            },
+            {
+                srNo: 5,
+                name: 'Mr. Ankit Raj',
+                position: 'TREASURER',
+                image: "imgLink",
+                linkedIn: "link",
+                facebook: "link",
+                email: "link",
+
+            },
+            {
+                srNo: 6,
+                name: 'Mr. Ishan',
+                position: 'PUBLIC RELATIONS OFFICER',
+                image: "imgLink",
+                linkedIn: "link",
+                facebook: "link",
+                email: "link",
+
+            },
+            {
+                srNo: 7,
+                name: 'Mr. Prabhat Ranjan',
+                position: 'SCHOOL DEVELOPMENT OFFICER',
+                image: "imgLink",
+                linkedIn: "link",
+                facebook: "link",
+                email: "link",
+
+            },
+            {
+                srNo: 8,
+                name: 'Mr. Kumar Aniket',
+                position: 'CHIEF FUNDRAISING OFFICER',
+                image: "imgLink",
+                linkedIn: "link",
+                facebook: "link",
+                email: "link",
+
+            },
+            {
+                srNo: 9,
+                name: 'Mr. Gursharan Ahir',
+                position: 'HUMAN RESOURCE MANAGER',
+                image: "imgLink",
+                linkedIn: "link",
+                facebook: "link",
+                email: "link",
+
+            },
+            {
+                srNo: 10,
+                name: 'Mr. Gaurav Singh',
+                position: ' EXECUTIVE OFFICER [RS]',
+                image: "imgLink",
+                linkedIn: "link",
+                facebook: "link",
+                email: "link",
+
+            },
+            {
+                srNo: 11,
+                name: 'Mr. Manoj Khojaram Maraskolhe',
+                position: 'CHIEF EXECUTIVE OFFICER [LiGHT]',
+                image: "imgLink",
+                linkedIn: "link",
+                facebook: "link",
+                email: "link",
+
+            },
+        ],
+        advisoryBody: [
+            {
+                srNo: 1,
+                name: "Prof. Adrijit Goswami",
+                position: "",
+                imageUrl: "/gyws/assets/img/new_website_img/members/Prof. Adrijit Goswami.jpg",
+                facebookLink: "",
+                linkedinLink: ""
+            },
+            {
+                srNo: 2,
+                name: "Prof. Amit Patra",
+                position: "",
+                imageUrl: "/gyws/assets/img/new_website_img/members/Prof. Amit Patra.jpg",
+                facebookLink: "",
+                linkedinLink: ""
+            },
+            {
+                srNo: 3,
+                name: "Dr. Bhaskar Bhowmik",
+                position: "",
+                imageUrl: "/gyws/assets/img/new_website_img/members/Dr. Bhaskar Bhowmik.jpg",
+                facebookLink: "",
+                linkedinLink: ""
+            },
+            {
+                srNo: 4,
+                name: "Prof. Damodar Maiti",
+                position: "",
+                imageUrl: "/gyws/assets/img/new_website_img/members/Prof. Damodar Maiti.jpg",
+                facebookLink: "",
+                linkedinLink: ""
+            },
+            {
+                srNo: 5,
+                name: "Prof. D.K. Maiti",
+                position: "",
+                imageUrl: "/gyws/assets/img/new_website_img/members/Prof. D.K. Maiti.jpg",
+                facebookLink: "",
+                linkedinLink: ""
+            },
+            {
+                srNo: 6,
+                name: "Prof. Dilip Swain",
+                position: "",
+                imageUrl: "/gyws/assets/img/new_website_img/members/Prof. Dilip Swain.jpg",
+                facebookLink: "",
+                linkedinLink: ""
+            },
+            {
+                srNo: 7,
+                name: "Retd. Prof. H.R. Tiwari",
+                position: "",
+                imageUrl: "/gyws/assets/img/new_website_img/members/dummy.png",
+                facebookLink: "",
+                linkedinLink: ""
+            },
+            {
+                srNo: 8,
+                name: "Prof. Sasmita Mohakud",
+                position: "",
+                imageUrl: "/gyws/assets/img/new_website_img/members/dummy.png",
+                facebookLink: "",
+                linkedinLink: ""
+            },
+            {
+                srNo: 9,
+                name: "Prof. Sudipta Datta",
+                position: "",
+                imageUrl: "/gyws/assets/img/new_website_img/members/dummy.png",
+                facebookLink: "",
+                linkedinLink: ""
+            },
+            {
+                srNo: 10,
+                name: "Prof. Sudipto Chakraborty",
+                position: "",
+                imageUrl: "/gyws/assets/img/new_website_img/members/Prof. Sudipto Chakraborty.jpg",
+                facebookLink: "",
+                linkedinLink: ""
+            },
+        ],
+        jagritiVidyaMandir: [
+            {
+                srNo: 1,
+                coordinators: [
+                    {
+                        srNo: 1,
+                        name: "",
+                        imageUrl: "link",
+                        facebookLink: "link",
+                        linkedinLink: "link",
+                        email: "link"
+                    },
+                    {
+                        srNo: 1,
+                        name: "",
+                        imageUrl: "link",
+                        facebookLink: "link",
+                        linkedinLink: "link",
+                        email: "link"
+                    },
+                    {
+                        srNo: 1,
+                        name: "",
+                        imageUrl: "link",
+                        facebookLink: "link",
+                        linkedinLink: "link",
+                        email: "link"
+                    },
+                ],
+                schoolReviewCommittee: [
+                    {
+                        srNo: 1,
+                        name: "",
+                        imageUrl: "link",
+                        facebookLink: "link",
+                        linkedinLink: "link",
+                        email: "link"
+                    },
+                    {
+                        srNo: 1,
+                        name: "",
+                        imageUrl: "link",
+                        facebookLink: "link",
+                        linkedinLink: "link",
+                        email: "link"
+                    },
+                    {
+                        srNo: 1,
+                        name: "",
+                        imageUrl: "link",
+                        facebookLink: "link",
+                        linkedinLink: "link",
+                        email: "link"
+                    },
+                ]
+            }
+        ]
+    }
+]
