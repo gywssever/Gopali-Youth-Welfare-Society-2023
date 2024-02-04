4200a9d44ba1ca8fcff59fcd27f0e2e91052a04a
Jatin Mahawar <jatinmahawar08@kgpian.iitkgp.ac.in>
2024-02-04 15:36:50 +0530

    .

---
+++ src/Components/Donation/Hostel_Contruction.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Hostel_Contruction() {
+    <script>setTimeout(function () { if (document.getElementById("ngoContentContainer").innerHTML.length < 40) { document.getElementById("ngoContentContainer").innerHTML = "<center> <p style='color:#a94442;'>we are sorry that our systems are down. we will be up shortly. apologies for the inconvenience.</p></center>"; } }, 20000);</script>
+
+  return (
+ <>
+ <div class="container-fluid">
+        <div class="row xrow" style="margin-left: 20px">
+            <div class="col-md-12" style="padding : 10px; font-size: 17px">
+                <script src="https://danamojo.org/dm/js/widget.js" type="text/javascript">
+                </script>
+                <div id="dmScriptContainer" style="display:none"><a href="">Donate Now</a></div>
+                <div id="ngoContentContainer" ingoid="640" odisplay="product" oLayout="LIST"
+                    iproductid="5018,5019,3995,3338,3339,3336,3340,3337,4583,6811" icustombuttonimage="no">
+                    <center><img alt="please wait..." src="https://danamojo.org/dm/css/images/loading.gif" /></center>
+                </div>
+                </script>
+            </div>
+        </div>
+    </div><br>
+
+
+
+ </>
+  )
+}
+
+export default Hostel_Contruction
