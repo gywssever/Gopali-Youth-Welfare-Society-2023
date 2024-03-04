63f65323d0137d0d492f7aaab945bd7871bb35e9
Ashutosh Kr Singh <ashutoshkrsinghsm@gmail.com>
2024-03-04 22:39:41 +0530

    Home Slider Height

--- src/Components/Homepage/slider/Slider.jsx
+++ src/Components/Homepage/slider/Slider.jsx
@@ -24,8 +24,11 @@ function Slider() {
           {
             carouselData.map((item, index) => {
               return (
-                <div key={index}>
-                  <img src={item.img} alt={item.alt} className='carousel_image' />
+                <div key={index} className='home-carousel-item'>
+                  <div className="home-carousel-item-image">
+
+                    <img src={item.img} alt={item.alt} className='carousel_image' />
+                  </div>
                   <p className="legend hide_slider">{item.legend}</p>
                 </div>
               )
