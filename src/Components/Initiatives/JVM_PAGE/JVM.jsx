238e33311a34759cd195ace7a56e982cde3bc1e7
Nishant Mishra <nishant202mishra@gmail.com>
2024-03-08 23:13:34 +0530

    done

--- src/Components/Initiatives/JVM_PAGE/JVM.jsx
+++ src/Components/Initiatives/JVM_PAGE/JVM.jsx
@@ -43,35 +43,35 @@ const eventsData = [
 const surveyData = [
   {
     number: 25,
-    info: "Teacher training sessions conducted",
+    info: "Teacher training sessions Conducted",
   },
   {
     number: 410,
-    info: "students",
+    info: "Students",
   },
   {
     number: 10,
-    info: "students rescued from child labour",
+    info: "Students Rescued from child Labour",
   },
   {
     number: 100,
-    info: "People attend the computer workshop",
+    info: "People attend the Computer Workshop",
   },
   {
     number: 25,
-    info: "cycles donated",
+    info: "Cycles Donated",
   },
   {
     number: 110,
-    info: "women attended the tailoring workshop",
+    info: "Women Attended the Tailoring workshop",
   },
   {
     number: 1000,
-    info: "potential blood donors database",
+    info: "Potential Blood Donors Database",
   },
   {
     number: 11,
-    info: "centers",
+    info: "Centers",
   },
 ];
 
@@ -97,7 +97,7 @@ const EventCard = ({ name, imgPath }) => {
       </div>
     </>
   ); 
-  
+
 };
 
 const SurveyStatsCard = ({ number, info }) => {
