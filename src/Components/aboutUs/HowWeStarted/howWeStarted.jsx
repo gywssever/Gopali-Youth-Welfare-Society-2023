9b203b49952db12d9b1082ca7a53095d71e861eb
Jatin Mahawar <jatinmahawar08@kgpian.iitkgp.ac.in>
2024-03-10 23:55:48 +0530

    Navlink added

--- src/Components/aboutUs/HowWeStarted/howWeStarted.jsx
+++ src/Components/aboutUs/HowWeStarted/howWeStarted.jsx
@@ -14,7 +14,7 @@ const HowWeStarted = () => {
             Mr. Mrinal Kanti Bhanja , or Dada as we call him , in his early twenties had the vision to give back to society by helping in uplifting the less fortunate . When he came across a few IITians who wanted to volunteer for a good cause but lacked the right platform- Gopali Youth Welfare Society came into being.
           </div>
           <div className='texts' >
-            Together, the Young Samaritans along with some Professors of IIT Kharagpur set on this journey and through the years GYWS has not just been instrumental in providing primary education to children at JVM, but has also undertaken different social initiatives to uplift the society! In Dada's own words “GYWS is aiding social, economic and cultural development of weaker sections of society while encouraging people to seek solutions to their everyday problems within locally available resources!”
+            Together, the Young Samaritans along with some Professors of IIT Kharagpur set on this journey and through the years GYWS has not just been instrumental in providing primary education to children at JVM, but has also undertaken different social initiatives to uplift the society! In Dada's own words <b> “GYWS is aiding social, economic and cultural development of weaker sections of society while encouraging people to seek solutions to their everyday problems within locally available resources!”</b>
           </div>
         </div>
         <div className="how-we-started-img">
