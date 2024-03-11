27199f2ac6d77585f64d4ad70bd5c36415d9e507
Nishant Mishra <nishant202mishra@gmail.com>
2024-03-11 17:31:40 +0530

    done

--- src/Components/Homepage/FeedIndia/feedindia.jsx
+++ src/Components/Homepage/FeedIndia/feedindia.jsx
@@ -16,9 +16,7 @@ export default function Guest() {
           </h2>
           <div className="descriptionGuest">
           We extend our deepest thanks to FeedIndia by Zomato for their invaluable support in providing food to children through Gyws, a student-led NGO from IIT Kharagpur. Your generosity nourishes young minds and fuels their dreams. Together, we're making a difference! #FeedIndia #Zomato #Gyws #ThankYou
-           </div>
-            We extend our deepest thanks to FeedIndia by Zomato for their invaluable support in providing food to children through Gyws, a student-led NGO from IIT Kharagpur. Your generosity nourishes young minds and fuels their dreams. Together, we're making a difference! #FeedIndia #Zomato #Gyws #ThankYou
-          </div>
+        
         </div>
         <div className="guest-image">
           <img src={feedindiaimage} alt="#" />
